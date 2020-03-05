09dba4f60dcf7ebc12b4995b3c7ca956cc56c0f7
sam@debian.com <>
2020-03-05 09:08:18 -0500

    Actualizado desde lugar X a las 9:07 am el 5 de Marzo

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -5,13 +5,15 @@
  */
 package Controlador;
 
-import Modelo.Imagen;
 import Modelo.Modelo;
 import Vista.Vista;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JOptionPane;
 /**
  *
  * @author sam
@@ -25,35 +27,45 @@ public class Controlador implements ActionListener{
     
     
    
+
     
-    /**
-     *
-     */
-    
-    public Controlador(){
-      
-    
-        
-  
-    
-    
-    }
-    
-    public Controlador(Vista view, Modelo model, Imagen imagen){
+    public Controlador(Vista view, Modelo model){
         this.view=view;
         this.model=model;
        
         
         
         this.view.btn_ingresar.addActionListener(this);
+        
+        this.view.txt_texto.addKeyListener(new KeyAdapter() { 
+            
+            public void keyPressed(KeyEvent event){
+                if (event.getKeyCode()==KeyEvent.VK_ENTER) {
+                    
+                    model.accionarEvento(view);
+                 
+                    
+                    
+                    
+                }
+            }
+        });
+        
+        
         this.view.lbl_cerrar.addMouseListener(new MouseAdapter() {
         
+            
+            
       
             @Override
             public void mouseClicked(MouseEvent evt){
-                System.exit(0);
+                int respuesta=JOptionPane.showConfirmDialog(null, "¿Desea cerrar el programa?", "Confirmar", JOptionPane.YES_NO_OPTION);
+                if (respuesta==0) {
+                    System.exit(0);
+                }
+                
             
-            
+                       
             }
             
      
@@ -63,13 +75,13 @@ public class Controlador implements ActionListener{
         
         );
         
-         int ancho=this.view.pn_fondo.getWidth();
-    int alto=this.view.pn_fondo.getHeight();
-    String ruta="/img/dragon ball.jpg";
-    Imagen img= new Imagen(ancho,alto,ruta); 
-        
-    this.view.pn_fondo.add(img);
-    this.view.pn_fondo.repaint();
+//         int ancho=this.view.pn_fondo.getWidth();
+//    int alto=this.view.pn_fondo.getHeight();
+//    String ruta="/img/dragon ball.jpg";
+//    Imagen img= new Imagen(ancho,alto,ruta); 
+//        
+//    this.view.pn_fondo.add(img);
+//    this.view.pn_fondo.repaint();
         
         
        
@@ -93,70 +105,21 @@ public class Controlador implements ActionListener{
         
         if (e.getSource()==this.view.btn_ingresar) {
             
-                   if (this.view.txt_texto.getText().isBlank()) {
+                  this.model.accionarEvento(view);
                 
-                       this.view.lbl_mensaje.setText("La caja está en blanco, por favor introduzca una palabra");
-            }
-                 
-                   
-                   
-                   
-          
-           
-                   else{
-                   
-                         model.setTexto(view.txt_texto.getText());
-        
-                         model.eliminarEspacios(model.getTexto());
-             
-                         model.invertir();
-            
-             
-                 
-                 
-                         view.lbl_mensaje.setText(model.getInvertida());
-                 
-                 if (model.getInvertida().equals(model.getTexto2())){
-                     view.lbl_comprobado.setText("Palabra Palíndroma");
-           
-             }
-                 
-                  else{
-                     view.lbl_comprobado.setText("No es una palabra Palíndroma");
-          
-        
-             }
-           
-                 /*Volvemos las cadenas a su estado original que es cadenas en blanco*/
-                
-                 this.model.setTexto("");
-                 this.model.setTexto2("");
-                 this.model.setInvertida("");
-                   
-                   
-                   }       
-                
-               
-          
-            
-          
-            
-
-             
-              
-        
-        
-       
             
             
         }
         
-      
         
+        
+              
        
     
     }
 
+    
+     
 
 }
 
