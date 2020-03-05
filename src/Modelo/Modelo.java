5978f50298f6b0e56a4803af5cb49b199a3ed0de
sam@debian <>
2020-03-05 12:41:29 -0500

    Commiter:Sam Sepiol <sam@debian>

--- src/Modelo/Modelo.java
+++ src/Modelo/Modelo.java
@@ -10,50 +10,64 @@ import Vista.Vista;
 public class Modelo {
     
     private String texto="",invertida="",texto2="";
-    
 
-
-
-    
-    
-    
-    
+    /**
+     *
+     * @return
+     */
     public String getTexto() {
         return texto;
     }
 
-    
+    /**
+     *
+     * @param texto
+     */
     public void setTexto(String texto) {
         this.texto = texto;
     }
     
-    
+    /**
+     *
+     * @return
+     */
     public String getInvertida(){
     
         return invertida;
     }
     
-    
+    /**
+     *
+     * @param invertida
+     */
     public void setInvertida(String invertida){
     
         this.invertida=invertida;
     }
     
-    
-    
+    /**
+     *
+     * @return
+     */
     public String getTexto2(){
     
         return texto2;
     
     }
     
+    /**
+     *
+     * @param texto2
+     */
     public void setTexto2(String texto2){
         this.texto2=texto2;
     
     
     }
     
-    
+    /**
+     *
+     */
     public void invertirCadena(){
         
         for (int i =this.texto.length()-1; i >=0; i--) {
@@ -64,6 +78,10 @@ public class Modelo {
         
     }
     
+    /**
+     *
+     * @param texto
+     */
     public void eliminarEspacios(String texto){
         
         for (int i = 0; i < texto.length(); i++) {
@@ -74,22 +92,34 @@ public class Modelo {
     
     }   
     
-    
+    /**
+     *
+     * @param view
+     */
     public void accionarEvento(Vista view){
         
          if (view.txt_texto.getText().isBlank()) {
                 
                        view.lbl_mensaje.setText("La caja está en blanco, por favor introduzca una palabra");
                        view.lbl_comprobado.setText("");            }
-                 
-                   
-                   
+             
+              
+         
          else if (!view.txt_texto.getText().isBlank()) {
              
-                   if (esNumerico(texto)==true) {
-                       
-                       
-                        this.setTexto(view.txt_texto.getText());
+                  if (esNumerico(view.txt_texto.getText())==true) {
+                      
+                      
+                      
+                      view.lbl_mensaje.setText("Es numerico");
+                      view.lbl_comprobado.setText("");
+                      
+                 
+             } //fin if
+                  
+                  
+                  else{
+                      this.setTexto(view.txt_texto.getText());
         
                          this.eliminarEspacios(this.getTexto());
              
@@ -116,46 +146,52 @@ public class Modelo {
                  this.setTexto("");
                  this.setTexto2("");
                  this.setInvertida("");
+                      
+                      
+                  }
+                       
+                       
+                        
                        
                        
                        
                        
                  
-             } // fin if anidado que va después del else if
+             
                    
                    
-                   else{
-                       view.lbl_comprobado.setText("");
-                       view.lbl_mensaje.setText("La cadena es totalmente numérica, ingrese una palabra con caracteres no numéricos");
-                       
-                       
-                   }
+               
                       
                    
                    
                    }      //fin else if
-            
-        
-      
-          
-           
-                    
-        
-        
     }
     
     
-    public boolean esNumerico(String texto){
-        boolean resultado;
+    
+    /*Método para comprobar si la cadena completa es numérica. Solo sirve para cadenas completas, por tanto, si combinamos letras y números, lo leerá como una cadena común y corriente*/
+
+    /**
+     *
+     * @param texto
+     * @return
+     */
+
+    
+    public boolean esNumerico(String texto){ //Declaramos el método de tipo boolean al cual le pasamos como parámetro una cadena de texto
+        boolean resultado; //Variable de tipo boolean para almacenar el resultado de la conversión de la cadena a numero
+        
+        /***Encerramos dentro de bloque try catch para atrapar la excepción 
+        en caso de que la conversión de la cadena a número falle***/
         try {
-            Integer.parseInt(texto);
-            resultado= true;
-        } catch (NumberFormatException e) {
+            Integer.parseInt(texto); //Hacemos la conversión o parseo de la cadena;
+            resultado= true; //Si resulta bien, el resultado será true
+        } catch (NumberFormatException e) { //En caso de que ocurra la excepción en el parseo, atrapamos la excepción del formateo de números y el resultado será false
             resultado= false;
             
         }
         
-        return resultado;
+        return resultado; /***Nos devuelve el resultado, sea falso o verdadero***/
     }
     
 }
